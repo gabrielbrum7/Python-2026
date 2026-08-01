c0bdf46d7ba40323eba186033c65204c54172f42
Gabriel Brum <53318145+gabrielbrum7@users.noreply.github.com>
2026-07-31 21:22:31 -0300

    Add files via upload

---
+++ 05metragem-para-cm.py
@@ -0,0 +1,7 @@
+#05.Faça um programa que converta metros para centímetros:#
+
+metragem = float(input('Qual é a metragem? '))
+
+conversao_cm = metragem * 10
+
+print('A metragem convertida para centímetros é:', f"{conversao_cm}cm")
---
+++ 06area-de-um-circulo.py
@@ -0,0 +1,7 @@
+#Faça um programa que peça o raio de um círculo, calcule e mostre sua área:#
+
+raio = float(input('Qual é o raio do círculo? '))
+
+area = 3.14 * (raio * raio)
+
+print('A área do círculo é:', area)
